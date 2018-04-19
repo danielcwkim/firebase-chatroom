3475c704407e2dabe46418f54f8e9a4d46d35e2f
danielkim <danielkimcw@gmail.com>
2018-04-19 13:15:49 -0700

    chatroom1

---
+++ src/components/RoomList.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react';
+
+
+class RoomList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state  = {
+      rooms: [],
+      newRoomName: ''
+    };
+
+    this.roomsRef = this.props.firebase.database().ref('rooms');
+
+  }
+
+  createRoom(newRoomName) {
+    this.roomsRef.push({
+      name: newRoomName
+    });
+      this.setState({ newRoomName: ''});
+  }
+
+  handleChange(e) {
+    this.setState({ newRoomName: e.target.value });
+  }
+
+
+  componentDidMount() {
+    this.roomsRef.on('child_added', snapshot => {
+      const room = snapshot.val();
+      room.key = snapshot.key;
+      this.setState({ rooms: this.state.rooms.concat( room )});
+    });
+  }
+
+  render(){
+    return (
+      <div className ="Room-list">
+        <ul>
+        {this.state.rooms.map((room, index) =>
+          <li key={index}>{room.name}</li>
+        )}
+        </ul>
+
+        <form onSubmit={ (e) => { e.preventDefault(); this.createRoom(this.state.newRoomName) }}>
+          <input type="text" value={ this.state.newRoomName } placeholder="Create a new room name" onChange= { (e) => this.handleChange(e) } />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RoomList;
