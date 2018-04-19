db8ade40e8d37e4aaa23091e16db96e47d41252a
danielkim <danielkimcw@gmail.com>
2018-04-19 13:18:15 -0700

    chatroom2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList';
+
+  var config = {
+    apiKey: "AIzaSyC_nLo0GB5mlO3lsfgQ6tP-gGtMT8942QU",
+    authDomain: "fbasecht.firebaseapp.com",
+    databaseURL: "https://fbasecht.firebaseio.com",
+    projectId: "fbasecht",
+    storageBucket: "fbasecht.appspot.com",
+    messagingSenderId: "164539777268"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header>
+          <h1>Bloc Chat</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <main>
+          <RoomList firebase={ firebase } />
+        </main>
       </div>
     );
   }
